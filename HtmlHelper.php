6cb752bc4c8e943db5068114045341a98125829b
basic-app <dev@basic-app.com>
2020-10-12 22:38:32 +0300

    n/a

---
+++ HtmlHelper.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018-2020 Basic App Dev Team
+ * @link https://basic-app.com
+ * @license MIT License
+ */
+namespace BasicApp\HtmlHelper;
+
+class HtmlHelper extends \PhpTheme\HtmlHelper\HtmlHelper
+{
+}
